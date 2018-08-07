0fde1ec0f11a2b364a595eb3b36cf467f5455173
tomkyle <412560+tomkyle@users.noreply.github.com>
2018-08-07 13:38:38 +0200

    Improve finding min and max values

--- src/MinMaxArrayIterator.php
+++ src/MinMaxArrayIterator.php
@@ -6,18 +6,11 @@ class MinMaxArrayIterator extends \ArrayIterator implements MinMaxInterface
 
     public function max()
     {
-        $max = $this->current();
-
-        foreach($this as $m)
-            $max = max($m, $max);
-        return $max;
+        return max( $this->getArrayCopy());
     }
 
     public function min()
     {
-        $min = $this->current();
-        foreach($this as $m)
-            $min = min($m, $min);
-        return $min;
+        return min( $this->getArrayCopy());
     }
 }
