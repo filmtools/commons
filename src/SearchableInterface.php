b76f5fa03612537e16edefa96d08b43f5f16806e
tomkyle <412560+tomkyle@users.noreply.github.com>
2018-08-15 10:18:20 +0200

    Add docblock

--- src/SearchableInterface.php
+++ src/SearchableInterface.php
@@ -4,6 +4,12 @@ namespace FilmTools\Commons;
 interface SearchableInterface
 {
     /**
+     * Searches the internal array for the given value
+     * and returns the first corresponding key if successful.
+     *
+     * This method uses PHP's array_search:
+     * @see http://php.net/manual/en/function.array-search.php
+     *
      * @param  float $value
      * @return int|false
      */
